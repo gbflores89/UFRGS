49b050f67e6da7b5cf6eb331a870be72726b1908
Guilherme <gbflores89@gmail.com>
2016-03-30 18:45:14 -0300

    Resultados

---
+++ Otimiza/trab2/EHL-6-48/Script.m
@@ -0,0 +1,16 @@
+clc; clear; % limpar console
+fun = @Problema; % definição do problema
+x0 = [485000,185000]; % valor inicial
+fval0 = fun(x0); % função objetivo avaliado no valor inicial 
+opt = optimset('fminunc');
+opt = optimset(opt,'Display','iter'); 
+opt = optimset(opt,'GradObj','off'); % definindo Quasi-Newton line search
+[x,fval,exitflag,output,grad,hessian] = fminunc(fun,x0,opt)
+opt = optimset(opt,'GradObj','on'); % definindo trust-region Newton
+[x,fval,exitflag,output,grad,hessian] = fminunc(fun,x0,opt)
+
+
+
+
+
+
