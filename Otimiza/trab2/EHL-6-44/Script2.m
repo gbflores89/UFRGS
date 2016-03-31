ef57445c730d2600bdf0d6bfe7cc9b9c5b5df4ea
Guilherme <gbflores89@gmail.com>
2016-03-30 22:23:15 -0300

    Exercício 6.48

---
+++ Otimiza/trab2/EHL-6-44/Script2.m
@@ -0,0 +1,15 @@
+clc; clear; % limpar console
+fun = @ObjFunc; % definição do problema
+x0 = [485000,185000]; % valor inicial
+fval0 = fun(x0); % função objetivo avaliado no valor inicial 
+opt = optimset('fminunc');
+opt = optimset(opt,'Display','iter'); 
+opt = optimset(opt,'GradObj','off'); % definindo Quasi-Newton line search
+[x,fval,exitflag,output,grad,hessian] = fminunc(fun,x0,opt)
+eigHessian = eig(hessian)
+
+
+
+
+
+
