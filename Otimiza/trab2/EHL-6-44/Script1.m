fbef4e0e3f0a518c315752dc39dc7b0ae314ff95
Guilherme <gbflores89@gmail.com>
2016-04-01 17:57:35 -0300

    add gráfico

--- Otimiza/trab2/EHL-6-44/Script1.m
+++ Otimiza/trab2/EHL-6-44/Script1.m
@@ -1,7 +1,6 @@
 clc; clear; % limpar console
 fun = @ObjFunc; % definição do problema
 x0 = 0; % valor inicial
-fval0 = fun(x0); % função objetivo avaliado no valor inicial 
 opt = optimset('fminunc');
 opt = optimset(opt,'Display','iter'); 
 opt = optimset(opt,'GradObj','on'); % definindo trust-region Newton
