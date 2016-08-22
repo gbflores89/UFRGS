8f4f6cca92c868bad1b926e64ceacdfc1e562bb0
Guilherme <gbflores89@gmail.com>
2016-08-22 17:55:06 -0300

    ex4

--- Otimiza/Script2.m
+++ Otimiza/Script2.m
@@ -2,7 +2,7 @@ clc; % limpar console
 clear; % limpar variáveis
 
 % Condição dos valoes iniciais: 1 = [0..2]; 2 = [-2..4]
-inicialValues=2;
+inicialValues = 1;
 
 % Algoritmos
 iFinal = 4;
@@ -16,7 +16,7 @@ jDim = zeros(1,jFinal);
 Ni = zeros(1,iFinal);
 % Tempo computacional para resolver o problema j utilizando o algoritmo i (100x)
 Tij = zeros(iFinal,jFinal);
-% Número total de iterações resolver o problema j utilizando o algoritmo i (100x)
+% Número total de avaliações da função objetivo para resolver o problema j utilizando o algoritmo i (100x)
 Sij = zeros(iFinal,jFinal);
 % Melhor valor da Função Objetivo encontrado (em 100x)
 Optij = zeros(iFinal,jFinal);
@@ -29,10 +29,10 @@ epsilonx = 1e-4; % (default)
 epsilonS = 1e-4; % (default)
 
 % Valor da Função objetivo no ótimo do problema j
-OtStar = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; -3.25 ];
+OtStar = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; -3.25];
 
-% Local do òtimo do problema j
-xijStar = [1 1 0 0; 1 1 0 0; 1 1 0 0; 1 1 0 0; 3 0.5 0 0; 1 1 1 1; 0 0 0 0; 0 1 1 1; 1 1 0 0; 0 0 0 0; 0.5 -1 0 0; 5 6 0 0; 2.5 0 0 0 ];
+% Local do ótimo do problema j
+xijStar = [1 1 0 0; 1 1 0 0; 1 1 0 0; 1 1 0 0; 3 0.5 0 0; 1 1 1 1; 0 0 0 0; 0 1 1 1; 1 1 0 0; 0 0 0 0; 0.5 -1 0 0; 5 6 0 0; 2.5 0 0 0];
 
 % Qualidade solução do problema j utilizando o algoritmo i
 dij = zeros(iFinal,jFinal);
@@ -85,38 +85,38 @@ for j = 1:jFinal
             end
             
             % valor inicial para cada função
-            if(j==6||j==7||j==8)
+            if (j==6||j==7||j==8)
                 x0=[rand1 rand2 rand3 rand4];
-                jDim(j)=4;
+                jDim(j) = 4;
             else
                 x0=[rand1 rand2];
-                jDim(j)=2;
+                jDim(j) = 2;
             end
             
             try
-                if i==1
-                    [xo,Ot,nS]=hkjeeves(S,x0,[],[],[],[],[],1e5);
+                if i == 1
+                    [xo,Ot,nS] = hkjeeves(S,x0,[],[],[],[],[],1e5);
                 end
-                if i==2
-                    [xo,Ot,nS]=cgrad(S,x0,[],G);
+                if i == 2
+                    [xo,Ot,nS] = cgrad(S,x0,[],G);
                 end
-                if i==3
-                    [xo,Ot,nS]=newton(S,x0,[],G,H);
+                if i == 3
+                    [xo,Ot,nS] = newton(S,x0,[],G,H);
                 end
-                if i==4
-                    [xo,Ot,nS]=lmarqua(S,x0,[],G,H);
+                if i == 4
+                    [xo,Ot,nS] = lmarqua(S,x0,[],G,H);
                 end
             catch
                 error = 1;
             end
             
             % Soma do número total de iterações
-            nS1 = nS1+nS;
+            nS1 = nS1 + nS;
             
             % Salvar o melhor valor ótimo do método entre as n iterações
-            if (opt>Ot)
+            if opt > Ot
                 opt = Ot;
-                for k=1:4
+                for k = 1:4
                     try
                         x1(k) = xo(k);
                     catch
@@ -126,12 +126,12 @@ for j = 1:jFinal
             end
         end
         
-        if (error == 0)
+        if error == 0
             % Caso o método execute normalmente
             time = toc;
             Tij(i,j) = time;
             Sij(i,j) = nS1;
-            Ni(i) = Ni(i)+1;
+            Ni(i) = Ni(i) + 1;
             Optij(i,j) = opt;
         else
             % Caso o método não execute o problema
@@ -139,64 +139,63 @@ for j = 1:jFinal
             Sij(i,j) = Inf;
             Optij(i,j) = Inf;
         end
-        dij(i,j) = norm (x1-xijStar(j,:))/epsilonx + norm((Optij(i,j)-OtStar(j)),1)/epsilonS;
+        dij(i,j) = norm (x1 - xijStar(j,:)) / epsilonx + norm((Optij(i,j) - OtStar(j)),1) / epsilonS;
     end
 end
 
-clc;
+clc; % limpar console
 
 %-------------------------------------------%
 % Eficácia (theta)
 theta = zeros(1,iFinal);
 TjStar = min(Tij,[],2);
-for i=1:iFinal
+for i = 1:iFinal
     t = 0;
-    for j=1:jFinal
-        sum1 = TjStar(i)/Tij(i,j);
+    for j = 1:jFinal
+        sum1 = TjStar(i) / Tij(i,j);
         t = t+sum1;
     end
-    theta(i) = (100/jFinal)*t;
+    theta(i) = (100 / jFinal) * t;
 end
 
 %-------------------------------------------%
 % Eficiência (chi)
 chi = zeros(1,iFinal);
 SjStar = min(Sij,[],2);
-for i=1:iFinal
+for i = 1:iFinal
     t = 0;
-    for j=1:jFinal
-        sum2 = + SjStar(i)/Sij(i,j);
-        t = t+sum2;
+    for j = 1:jFinal
+        sum2 = + SjStar(i) / Sij(i,j);
+        t = t + sum2;
     end
-    chi(i) = (100/jFinal)*t;
+    chi(i) = (100 / jFinal) * t;
 end
 
 %-------------------------------------------%
 % Robustez (eta)
 eta = zeros(1,iFinal);
-for i=1:iFinal
-    eta(i) = 100*(sum(Ni(i))/jFinal);
+for i = 1:iFinal
+    eta(i) = 100 * (sum(Ni(i)) / jFinal);
 end
 
 %-------------------------------------------%
 % Qualidade da solução obtida (xi)
 xi = zeros(1,iFinal);
 djStar = min(dij,[],2);
-for i=1:iFinal
+for i = 1:iFinal
     t = 0;
-    for j=1:jFinal
-        sum3 = + (djStar(i)+epsilon)/(dij(i,j)+epsilon);
-        t = t+sum3;
+    for j = 1:jFinal
+        sum3 = + (djStar(i) + epsilon) / (dij(i,j) + epsilon);
+        t = t + sum3;
     end
-    xi(i) = (100/jFinal)*t;
+    xi(i) = (100 / jFinal) * t;
 end
-
 
 %-------------------------------------------%
 % Métrica para o melhor método
 total = zeros(1,iFinal);
-for i=1:iFinal
-    total(i) = theta(i)+chi(i)+eta(i)+xi(i);
+for i = 1:iFinal
+    total(i) = theta(i) + chi(i) + eta(i) + xi(i);
 end
 
 disp(theta)
--- Otimiza/trab2/EHL-6-44/ObjFunc.m
+++ Otimiza/trab2/EHL-6-44/ObjFunc.m
@@ -1,6 +1,6 @@
 function [f,g] = ObjFunc(x)
 % Calculate objective f
-f = [93750.0000-46875.00000*sqrt(2)*sqrt(pi)*(2000*erf((1/4000)*x*sqrt(2))+sqrt(4000000))]+(3750.000000*((1/2)*x+(1/2)*pi))*sqrt(2)*sqrt(pi)*(2000*erf((1/4000)*x*sqrt(2))+sqrt(4000000))*((1/2)*x+(1/4)*sqrt(2)*sqrt(pi));
+f = [93750.0000 - 46875.00000*sqrt(2)*sqrt(pi)*(2000*erf((1/4000)*x*sqrt(2)) + sqrt(4000000))] + (3750.000000*((1/2)*x + (1/2)*pi))*sqrt(2)*sqrt(pi)*(2000*erf((1/4000)*x*sqrt(2)) + sqrt(4000000))*((1/2)*x + (1/4)*sqrt(2)*sqrt(pi));
 if nargout > 1 % gradient required
     g = ObjFuncGrad(x);
 end
